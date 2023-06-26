cbc45bf188ca21e4189680855e964c45a022fea8
Flavio <alvaradotucto@gmail.com>
2023-06-26 10:08:49 -0500

    redeplow

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var compression = require('compression');
 require('dotenv').config();
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,6 +17,9 @@ app.use('/api/get-uuid', (req, res) =>{
   res.status(200).json({
     uuid: uuidv4()
   })
+});
+app.use('/', (req, res) => {
+  res.sendFile(__dirname, '/public/app.html');
 });
 
 app.use('*', (req, res) => {
@@ -25,6 +30,4 @@ const IP = process.env.NODE_ENV === 'production' ? process.env.IP_PROD : process
 
 const serverHttp = createServer(app);
 
-serverHttp.listen(process.env.HTTP_PORT, () => {
-  console.log(`server running on port ${process.env.HTTP_PORT}`);
-});
+serverHttp.listen(process.env.HTTP_PORT);
